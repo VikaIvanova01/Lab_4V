262c0861454a16116c79dab6179422607f69e382
Виктория Иванова <viktoriiaivanova101@gmail.com>
2020-11-19 20:29:01 +0800

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRefill = new System.Windows.Forms.Button();
+            this.txtInfo = new System.Windows.Forms.RichTextBox();
+            this.txtOut = new System.Windows.Forms.RichTextBox();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.listOfDrinks = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnRefill
+            // 
+            this.btnRefill.Location = new System.Drawing.Point(23, 26);
+            this.btnRefill.Name = "btnRefill";
+            this.btnRefill.Size = new System.Drawing.Size(282, 31);
+            this.btnRefill.TabIndex = 0;
+            this.btnRefill.Text = "Перезаполнить";
+            this.btnRefill.UseVisualStyleBackColor = true;
+            this.btnRefill.Click += new System.EventHandler(this.btnRefill_Click);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtInfo.Location = new System.Drawing.Point(23, 63);
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.Size = new System.Drawing.Size(282, 34);
+            this.txtInfo.TabIndex = 1;
+            this.txtInfo.Text = "";
+            // 
+            // txtOut
+            // 
+            this.txtOut.Location = new System.Drawing.Point(23, 112);
+            this.txtOut.Name = "txtOut";
+            this.txtOut.Size = new System.Drawing.Size(191, 146);
+            this.txtOut.TabIndex = 2;
+            this.txtOut.Text = "";
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(231, 112);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(74, 146);
+            this.btnGet.TabIndex = 3;
+            this.btnGet.Text = "Взять";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // listOfDrinks
+            // 
+            this.listOfDrinks.Location = new System.Drawing.Point(357, 28);
+            this.listOfDrinks.Name = "listOfDrinks";
+            this.listOfDrinks.Size = new System.Drawing.Size(158, 230);
+            this.listOfDrinks.TabIndex = 4;
+            this.listOfDrinks.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 289);
+            this.Controls.Add(this.listOfDrinks);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.btnRefill);
+            this.Name = "Form1";
+            this.Text = "Автомат по выдаче напитков";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRefill;
+        private System.Windows.Forms.RichTextBox txtInfo;
+        private System.Windows.Forms.RichTextBox txtOut;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.RichTextBox listOfDrinks;
+    }
+}
+
